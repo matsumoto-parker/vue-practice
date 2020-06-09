f270ad6379e9c318d04e66ee76a0085a1e000ae2
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-06-09 16:04:53 +0900

    feat: start app

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="main-container">mainが入りますね</main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-container {
+  width: calc(100% - 200px);
+  flex: 1;
+  position: absolute;
+  left: 200px;
+}
+</style>
