03cc42c018fd6cca0e35e205cb0e78b97e905ed0
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-06-09 13:04:15 +0900

    feat: add button and counter

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="button" @click="$emit('onClick', $event)">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  display: inline-block;
+  background: red;
+  border-radius: 5px;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  margin-right: 5px;
+}
+
+.button:hover {
+  background: rgb(182, 0, 0);
+}
+
+.button:active {
+  background: rgb(138, 13, 13);
+}
+</style>
