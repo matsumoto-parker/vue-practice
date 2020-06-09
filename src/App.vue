135ed414d09b53c977bd58cd11351da6c39a4ec0
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-06-09 17:41:09 +0900

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .app-container {
   display: flex;
   padding-top: 60px;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,47 @@
 <template>
-  <main class="main-container">mainが入りますね</main>
+  <main class="main-container">
+    <TextBox />
+    <div class="devider"></div>
+    <MessageList :messages="messages" />
+    mainが入りますね
+  </main>
 </template>
 
 <script>
-export default {};
+import TextBox from './TextBox';
+import MessageList from './MessageList';
+
+export default {
+  components: {
+    TextBox,
+    MessageList,
+  },
+  data() {
+    return {
+      messages: [
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+        { date: 'dddddd', body: 'bbbbbbbb' },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .main-container {
   width: calc(100% - 200px);
   flex: 1;
   position: absolute;
   left: 200px;
+}
+
+.devider {
+  border-top: 10px solid #ccc;
 }
 </style>
